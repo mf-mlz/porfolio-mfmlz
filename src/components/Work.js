753479ad5f9efc4f9f44b4cdeb1e14b7144b7485
“Fernanda <maryfermtzlb@gmail.com>
2023-07-22 12:55:32 -0600

    fix(Se agrega texto)

--- src/components/Work.js
+++ src/components/Work.js
@@ -51,7 +51,7 @@ const Work = ({ theme }) => {
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
               <img className='group-hover:scale-125 transition-all duration-500' src={theme === 'dark' ? Img2Dark : Img2} alt=''></img>
               <div className='absolute -bottom-full left-12 group-hover:bottom-24 transition-all duration-500 z-50'>
-                <span className='text-gradient font-poppins'>Exercise for consuming a REST API using promises,<br/> using JavaScript.</span>
+                <span className='text-gradient font-poppins'>Exercise for consuming a REST API using promises,<br/> using JavaScript / PHP in two differents projects.</span>
               </div>
               <div className='absolute -bottom-full left-12 group-hover:bottom-14 transition-all duration-700 z-50'>
                 <span className='text-3xl text-white font-poppins'>POKEDEX</span>
