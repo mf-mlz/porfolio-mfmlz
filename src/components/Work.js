11dc9de5a15fa1e07c36f11acb098e72617ad0e9
“Fernanda <maryfermtzlb@gmail.com>
2023-07-22 12:52:46 -0600

    fix(Se quita Botón)

--- src/components/Work.js
+++ src/components/Work.js
@@ -27,8 +27,8 @@ const Work = ({ theme }) => {
               <p className='max-w-lg mb-16 font-poppins text-black dark:text-white'>
               Among some small projects that I have done, they range from exercises to small pages made with Bootstrap or Tailwind CSS, also to practice consuming a REST API like Pokemon's, and a small Spotify made with Node JS, Mongo DB and Angular (which is still in development).
               </p>
-              <button className='btn btn-sm font-poppins'>
-                <a href='https://mf-mlz.github.io/mf-mlz/proyectos.html' target='_blank' rel="noopener noreferrer">View Projects</a></button>
+              {/* <button className='btn btn-sm font-poppins'>
+                <a href='https://mf-mlz.github.io/mf-mlz/proyectos.html' target='_blank' rel="noopener noreferrer">View Projects</a></button> */}
             </div>
             <div className='group relative overflow-hidden border-2 border-black/70 dark:border-white/50 rounded-xl h-96'>
               <div className='group-hover:bg-black/70 w-full h-full absolute z-40 transition-all duration-300'></div>
